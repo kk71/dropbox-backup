4da3901af1e7b53aa6645c497beb4a5097e604dc
kk <fkfkbill@gmail.com>
2014-01-27 14:11:07 +0800

    添加了systemd服务，以及daemon驻守一定时间间隔自动备份

--- README.md
+++ README.md
@@ -1,23 +1,45 @@
-dropbox_backup
+dropbox backup
 ==============
 
-备份文件或目录至dropbox
+backup files or folders into dropbox.
 
-### 环境要求
+### Environmet Requirement
 
 * bash
 * git
-* [dropbox_uploader](https://github.com/andreafabrizi/Dropbox-Uploader)(能自动下载)
+* [dropbox_uploader](https://github.com/andreafabrizi/Dropbox-Uploader)(auto-download supported)
 
-### 使用
-
-在~/.dropbox_backup中保存你需要备份的目录或文件绝对路径。
+### Installation
 
 ```bash
-    # cd
-    # git clone https://github.com/kk71/dropbox_backup.git
-    # cd dropbox_backup
-    # chmod +x backup.sh
-    # ./backup.sh
-    # vim ~/.dropbox_backup
+    #run in root user
+    cd
+    git clone https://github.com/kk71/dropbox_backup.git
+    cd dropbox_backup
+    chmod +x backup.sh
+    ./backup.sh
+    vim ~/.dropbox_backup #add folders or files to backup.
 ```
+
+
+### Usage
+
+'''bash
+    ./dropbox_backup.sh #install and initial, or show help
+    ./dropbox_backup.sh --help #show help
+
+    #run in root user
+    ./dropbox_backup.sh --daemon [backup interval] #run as a daemon,and backup accord to interval
+    ./dropbox_backup.sh --now #backup right now
+
+    #for systemd
+    ./dropbox_backup.sh --install-service [backup interval] #install as a service and start
+    ./dropbox_backup.sh --delete-service #delete service
+'''
+
+intervals are hours(h).
+
+### Notice
+
+system-wide proxy is supported. since dropbox is often unreacheable in some countries, it's recommanded to use a proxy or a wall-breaker to access dropbox.
+[GoAgent - a gae proxy forked from gappproxy/wallproxy](https://code.google.com/p/goagent/)
---
+++ dropbox_backup.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#coding=utf-8
+#backup files or folders into your dropbox
+#author:kk(http://imkk.me)
+#dependences & help please refer to README.md
+
+#configurations which needn't be changed only if you know what you're doing
+#must end up with "/"
+tmpdir="/tmp/dropbox-backup/"
+dropbox_dir="/autobackup/"
+dropbox_uploader_git_repo="https://github.com/andreafabrizi/Dropbox-Uploader.git"
+
+#show info in green font
+function put {
+    echo -e "\033[032m$@\033[m";
+}
+
+#test if git exists
+gitver=`git --version 2> /dev/null` 
+if [[ ${gitver:0:11} != "git version" ]];then
+    put "git error. please install latest git."
+    exit
+fi
+
+#test if dropbox_uploader exists
+#otherwise clone it from it's repo
+if ! [ -d "Dropbox-Uploader" ];then
+    git clone $dropbox_uploader_git_repo
+    if [ $? -ne 0 ];then
+        put "network error or git configuration error? please manually clone dropbox_uploader into local path."
+        put "dropbox_uploader git repo : $dropbox_uploader_git_repo"
+        exit
+    fi
+    put "dropbox_uploader is installed and now please configure it..."
+    put "==========================="
+    chmod +x Dropbox-Uploader/dropbox_uploader.sh
+    bash Dropbox-Uploader/dropbox_uploader.sh
+    put "==========================="
+    put "finished. now please fulfill ~/.dropbox_backup for files to backup.\\n"
+    exit
+fi
+
+#run as a daemon
+if [[ $1 == "--daemon" ]];then
+    put "dropbox backup is running as a daemon..."
+    while :;do
+        ./dropbox_backup.sh --now
+        sleep $2h
+    done
+    exit
+fi
+
+#create/delete and enable/disable a systemd service
+pwd=`pwd`
+pwd_slash_escaped=$(echo $pwd|sed 's/\//\\\//g') #escape pwd slashes
+if [[ $1 == "--install-service" ]];then
+    if [[ -z $2 ]];then
+        put "interval invalid."
+        exit
+    fi
+    sed "s/dropbox_backup_pwd/$pwd_slash_escaped/g;s/backup_interval/$2/g" dropbox_backup.service > /etc/systemd/system/dropbox_backup.service;
+    systemctl enable dropbox_backup.service
+    if [[ $? != "0" ]];then
+        put "failed. please make sure you're doing this with root previlege or in sudo."
+    fi
+    exit
+else if [[ $@ == "--delete-service" ]];then
+    systemctl disable dropbox_backup.service
+    rm /etc/systemd/system/dropbox_backup.service
+    if [[ $? != "0" ]];then
+        put "failed. please make sure you're doing this with root previlege or in sudo."
+    fi
+    exit
+fi fi
+
+#backup now
+if [[ $@ == "--now" ]];then
+    put "backup now..."
+    pwd=`pwd` #save current path
+    files_to_backup=`cat ~/.dropbox_backup 2>/dev/null`;
+    if [[ $files_to_backup == "" ]];then
+        put "no backup files defined in ~/.dropbox_backup."
+        exit
+    fi
+    rm -rf $tmpdir
+    mkdir $tmpdir
+    for f in $files_to_backup;do
+        cd `dirname $f`
+        tar -cjf ${tmpdir}${f//"/"/"-"}.tar.gz `basename $f`
+        if [ $? -ne 0 ];then
+            put "error at creating tarball for $f"
+        fi
+    done
+    put "uploading to dropbox..."
+    datenow=`date -d today +"%Y-%m-%d-%H:%M:%S"`
+    #-k switches ssl certificate check off(for system-wide proxy)
+    ${pwd}/Dropbox-Uploader/dropbox_uploader.sh -k upload $tmpdir ${dropbox_dir}$datenow
+    put "finished."
+    exit
+fi
+
+#if no arguments or with --help then show help info
+cat README.md
+if [ $? != "0" ];then
+    put "README.md not found."
+fi
